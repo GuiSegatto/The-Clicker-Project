bf94c760b08f48213d22f9424bf11fd1ef85351d
GuiSegatto <guisegatto23@gmail.com>
2022-12-13 20:09:41 -0300

    refactor: trying to make it oop

--- script.js
+++ script.js
@@ -1,103 +1,52 @@
-/* 
-        Variaveis
-*/
+/* Event Listeners */
+document.querySelector("#clickHere").addEventListener("click", () => { game.addToGold(game.clickPower) });
 
+let game = { // objeto onde ficá todos os nossos dados a serem salvos
+    currentGold: 0,                         // Gold Atual
+    totalGoldEarned: 0,                     // Gold TOTAL já adquirido, mesmo gastado não diminui
+    goldPerSecond: 0,                       // Gold Por Segundo
+    clickPower: 1,                          // Quantia de gold ganha por click
 
+    addToGold: function (amount) {            // Adiciona ao gold atual a quantia passada
+        this.currentGold += amount;         // atualiza o valor do gold atual
+        this.totalGoldEarned += amount;     // atualiza o valor do gold total já adquirido
+        display.updateGold();               // Executa a função dentro do objeto display que irá atualizar na tela o valor do gold atual
+    },
+};
 
-// Gold - Dinheiro
-let gold = 0;
-
-// Cursores
-let cursorCost = 15;
-let cursorAmount = 0;
-
-
-/* 
-    Event listeners
-*/
-
-// Toda vez que clickarmos no botão ele executa a função e adicionará 1 de gold
-document.querySelector("#clickHere").addEventListener("click", () => { addToGold(1) });
-// Toda vez que clickarmos no botão de comprar o cursor executa a função de comprar o cursor
-document.querySelector("#buyCursor").addEventListener("click", buyCursor)
-// quando clickarmos salva o save do jogo
-document.querySelector("#saveGame").addEventListener("click", saveGame)
-// quando clickar abre a janela de confirmação para resestar o jogo
-document.querySelector("#resetGame").addEventListener("click", resetGame)
-
-// Soma a quantidade passada ao nosso total de gold 
-function addToGold(amount) {
-    gold = gold + amount
-    document.querySelector("#totalGold").innerHTML = gold;
-}
-
-// Criada a função de compra de cursores
-function buyCursor() {
-    if (gold >= cursorCost) { // se o gold for maior ou igual ao valor do cursor então....
-        gold = gold - cursorCost; // define que o gold será o total de gold - o valor do cursor
-        cursorAmount = cursorAmount + 1; // Adiciona +1 para o total de cursores
-        cursorCost = Math.round(cursorCost * 1.15); // Atualiza o valor do cursor para 1.15 o que estavá custando e aredonda o valor final
-
-        document.querySelector("#totalGold").innerHTML = gold // atualiza o total de gold
-        document.querySelector("#cursorCost").innerHTML = cursorCost // atualiza o preço do cursor
-        document.querySelector("#cursorAmount").innerHTML = cursorAmount // atualiza o total de cursores
+let display = { // Objeto onde ficá todo que será mostrado na tela
+    updateGold: function () {
+        document.querySelector('#totalGold').innerText = game.currentGold
+        document.title = game.currentGold + " - The Waystone Clicker"
     }
-}
+};
 
-setInterval(function () {   // função de "DPS", atualiza o ouro ganho a cada 1 segundo
-    gold = gold + cursorAmount // para aumentar podemos adicionar um "* (y)"
-    document.querySelector("#totalGold").innerHTML = gold // atualiza o valor do gold a cada .....
-}, 1000) // 1000ms = 1secs
+let hero = {
+    name: [
+        "Fox",
+        "Eric",
+        "Nikasha",
+    ],
 
+    cost: [
+        10,
+        100,
+        1000,
+    ],
 
+    level: [
+        0, 0, 0
+    ],
+    income: [
+        1, 10, 100
+    ],
 
-/* 
-        Save game
-*/
-
-// Auto save
-setInterval(function () { // salva o jogo a cada 60 segundos
-    saveGame();
-}, 60000) //  60000ms = 60secs
-
-// Load Function
-function loadGame() {
-    let savedGame = JSON.parse(localStorage.getItem("gameSave")); // busca o arquivo JSON "gameSave"
-    if (typeof savedGame.gold !== "undefined") gold = savedGame.gold; // previne erros e garante que não irá atualizar o valor do gold se o valor for undefined
-    if (typeof savedGame.cursorCost !== "undefined") cursorCost = savedGame.cursorCost;
-    if (typeof savedGame.cursorAmount !== "undefined") cursorAmount = savedGame.cursorAmount;
-}
-
-// Save Function
-function saveGame() {
-    let gameSave = { // criamos um objeto que irá guardar nossos dados
-        gold: gold, // Onde irá guardar: oque irá guardar 
-        cursorCost: cursorCost,
-        cursorAmount: cursorAmount,
-    }
-    localStorage.setItem("gameSave", JSON.stringify(gameSave)); // salva através de cookies em um arquivo JSON de um modo string
-}
-
-window.onload = function () { // quando a pagina carrega executa essa função
-    loadGame(); // carrega o jogo
-    document.querySelector("#totalGold").innerHTML = gold // atualiza o total de gold
-    document.querySelector("#cursorCost").innerHTML = cursorCost // atualiza o preço do cursor
-    document.querySelector("#cursorAmount").innerHTML = cursorAmount // atualiza o total de cursores
-}
-
-// Previne que usuario use ctrl + S que abriria o save no navegador e salva executa a função de save ao invés disso
-document.addEventListener("keydown", function (event) { // tecla pressionada o event registra qual foi a tecla
-    if (event.ctrlKey && event.key == "s") { // se o CTRL e o S forem apertados
-        event.preventDefault(); // previne que abra a janela do browser de save
-        saveGame(); // salve o jogo
-    }
-}, false)
-
-// Function Reset Game
-function resetGame() {
-    if (confirm("Tem certeza que deseja resetar o save do jogo? \n \nATENÇÃO: Isso não trará nenhum benificio adicional")) {
-        let gameSave = {};
-        localStorage.setItem("gameSave", JSON.stringify(gameSave));
-        location.reload(); // Garante que assim que confirmar irá recarregar a pagina assim executando a função "window.onload"
+    purchaseHero: function (index) {                                     // Função para comprar os heróis baseado na posição do index
+        if (game.currentGold >= this.cost[index]) {                     // Se o preço for menor que o gold atual então...
+            game.currentGold -= this.cost[index];                      // Diminui o valor do gold atual com opreço do heroi
+            this.level[index] += 1;                                   // Aumenta 1 level do herói
+            this.cost[index] = Math.round(this.cost[index] * 1.15);  // aumenta o custo do herói basicamente a cada 5 herois dobra o preço
+            display.updateGold();
+        }
     }
 }
