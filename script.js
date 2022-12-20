e58acf6b5913dba2249c7b6d87a5c506a1767441
GuiSegatto <guisegatto23@gmail.com>
2022-12-20 20:59:23 -0300

    added shop menu via js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 /* Event Listeners */
-document.querySelector("#clickHere").addEventListener("click", () => { game.addToGold(game.clickPower) });
-document.getElementById("buyHero").addEventListener("click", () => { hero.purchaseHero(0) })
+document.querySelector("#clickHere").addEventListener("click", () => { game.addToGold(game.clickPower), display.clicked() });
 
-/* 
-    Objeto onde ficá todos os nossos dados a serem salvos
-    E as funcionalidades do jogo
-*/
+
+
+
 let game = {
     currentGold: 0,                         // Gold Atual
     totalGoldEarned: 0,                     // Gold TOTAL já adquirido, mesmo gastado não diminui
@@ -20,30 +18,47 @@ let game = {
     getGoldPerSecond: function () {
         let goldPerSecond = 0
         for (index = 0; index < hero.name.length; index++) { // o index começa no 0, enquanto o index for menor que o tamanho de name, executa e aumenta 1 no numero do index
-            goldPerSecond += hero.income[index] * hero.level[index] // irá percorrer nosso array atribuindo ao GpS a quantia de gold que o heroi dá X o seu level
+            goldPerSecond += hero.baseIncome[index] * hero.level[index] // irá percorrer nosso array atribuindo ao GpS a quantia de gold que o heroi dá X o seu level
         };
         return goldPerSecond; // retorna essa variavel para o lugar onde a função foi chamada
     },
 };
 
 
-/* 
-    Objeto onde ficá todo que será mostrado na tela
-*/
+
 let display = {
     updateGold: function () {
         document.querySelector('#currentGold').innerText = game.currentGold;
         document.querySelector('#goldPerSecond').innerText = game.getGoldPerSecond();
         document.title = game.currentGold + " - The Waystone Clicker";
     },
-    updateHeroInfo: function () {
-        document.getElementById('heroLevel').innerText = hero.level[0];
-        document.getElementById('heroIncome').innerText = hero.level[0] * hero.income[0];
-        document.getElementById('heroCurrentPrice').innerText = hero.cost[0];
+
+    updateShop: function() {
+        document.querySelector('#heroShop').innerHTML = "";
+        for (index = 0; index < hero.name.length; index++) {
+            document.querySelector('#heroShop').innerHTML += '<div class="hero-wrapper"><div class="hero-img" id="heroImage">hero img</div><div class="hero-name-wrapper"><h3>'+hero.name[index]+'</h3><div class="hero-upgrades">0 0 0 0 0</div></div><div class="hero-info"><p class="hero-level">Level: <span>'+hero.level[index]+'</span></p><p class="hero-income">Base Income: <span>'+hero.baseIncome[index]+'</span></p></div><button class="buy-hero animate-'+index+'" onclick="hero.purchaseHero('+index+')">Level-Up<div>'+hero.cost[index]+'</div></button></div>'
+        }
+    },
+
+    buyAnimation: function (index) {
+        let button = document.querySelector(`.animate-${index}`);
+        button.classList.add('glow')
+        setTimeout(() => {
+            button.classList.remove('glow');
+        }, 120)
+    },
+
+    clicked: function () {
+        let clickHere = document.querySelector(".click-here-text")
+        clickHere.classList.add('clicked')
+        setTimeout(() => {
+            clickHere.classList.remove('clicked')
+        }, 120)
     }
+
 };
 
-// Função de gold por segundo
+// Gera o Gold Por Segundo
 setInterval(function () {
     game.currentGold += game.getGoldPerSecond();
     game.totalGoldEarned += game.getGoldPerSecond;
@@ -57,38 +72,32 @@ setInterval(function () {
 window.onload = function () {
     loadGame();
     display.updateGold();
-    display.updateHeroInfo();
+    display.updateShop();
 }
-
-
 
 /* 
     SAVE GAME
 */
-
-// Salva o jogo
 function saveGame() {
-    let gameSave = { // criamos um objeto que irá guardar nossos dados
+    let gameSave = { 
         currentGold: game.currentGold,
         totalGoldEarned: game.totalGoldEarned,
         clickPower: game.clickPower,
         heroCost: hero.cost,
         heroLevel: hero.level,
-        heroIncome: hero.income,
+        heroBaseIncome: hero.baseIncome,
     }
-    localStorage.setItem("gameSave", JSON.stringify(gameSave)); // salva através de cookies em um arquivo JSON de um modo string
+    localStorage.setItem("gameSave", JSON.stringify(gameSave)); 
 }
-
-// Carrega o jogo
 function loadGame() {
-    let savedGame = JSON.parse(localStorage.getItem("gameSave")); // busca o arquivo JSON "gameSave"
-    if (localStorage.getItem("gameSave") !== null) { // se o save existir então...
-        if (typeof savedGame.currentGold !== "undefined") game.currentGold = savedGame.currentGold; // previne erros e garante que não irá atualizar o valor do gold se o valor não estiver definido
+    let savedGame = JSON.parse(localStorage.getItem("gameSave")); 
+    if (localStorage.getItem("gameSave") !== null) { 
+        if (typeof savedGame.currentGold !== "undefined") game.currentGold = savedGame.currentGold; 
         if (typeof savedGame.totalGoldEarned !== "undefined") game.totalGoldEarned = savedGame.totalGoldEarned;
         if (typeof savedGame.clickPower !== "undefined") game.clickPower = savedGame.clickPower;
         if (typeof savedGame.heroCost !== "undefined") hero.cost = savedGame.heroCost;
         if (typeof savedGame.heroLevel !== "undefined") hero.level = savedGame.heroLevel;
-        if (typeof savedGame.heroIncome !== "undefined") hero.income = savedGame.heroIncome;
+        if (typeof savedGame.heroBaseIncome !== "undefined") hero.baseIncome = savedGame.heroBaseIncome;
     }
 }
 
@@ -118,9 +127,6 @@ function resetGame() {
 }
 
 
-/* 
-    Onde ficará nossos personagens que podem ser adquiridos
-*/
 let hero = {
     name: [
         "Fox",
@@ -137,17 +143,19 @@ let hero = {
     level: [
         0, 0, 0
     ],
-    income: [
+    baseIncome: [
         1, 10, 100
     ],
 
-    purchaseHero: function (index) {                                     // Função para comprar os heróis baseado na posição do index
-        if (game.currentGold >= this.cost[index]) {                     // Se o preço for menor que o gold atual então...
-            game.currentGold -= this.cost[index];                      // Diminui o valor do gold atual com opreço do heroi
-            this.level[index] += 1;                                   // Aumenta 1 level do herói
+    purchaseHero: function (index) {                                    
+        if (game.currentGold >= this.cost[index]) {                     
+            game.currentGold -= this.cost[index];                     
+            this.level[index] += 1;                                   
             this.cost[index] = Math.round(this.cost[index] * 1.15);  // aumenta o custo do herói basicamente a cada 5 herois dobra o preço
             display.updateGold();
-            display.updateHeroInfo();
+            display.updateShop();
+            display.buyAnimation(index);
+            
         }
     }
 }
